ba79569653fabf52f613df7fc5f827735f93b14a
mehdifeshki <mahdifeshki08@gmail.com>
2025-04-16 22:07:58 +0330

    Update p3.cpp

--- p3.cpp
+++ p3.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class Student {
+private:
+    int id;
+    string firstName;
+    string lastName;
+    float grade;
+public:
+    Student(int id, string firstName, string lastName, float grade)
+        : id(id), firstName(firstName), lastName(lastName), grade(grade) {}
+    float getGrade() const {
+        return grade;}}
+class Teacher {
+private:
+    int id;
+    string firstName;
+    string lastName;
+    float salary;
+public:
+    Teacher(int id, string firstName, string lastName, float salary)
+        : id(id), firstName(firstName), lastName(lastName), salary(salary) {}
+   float getSalary() const {
+        return salary;}}
+
+
+
